f2ea24ae40dca3ac7a640dafd07cac7f1bbaf615
Roman Kirsanov <kirsanov2008@gmail.com>
2024-01-30 22:45:18 -0600

    Update shared.ts

--- src/shared.ts
+++ src/shared.ts
@@ -9,49 +9,38 @@ export type Shared<T> = {
     off: (listener: SharedListener<T>) => void;
 }
 
-export const createShared = <T>(defaultValue: T): Shared<T> => {
+export const createShared = <T>(default_: T): Shared<T> => {
+    let _value: T = default_;
     let _subs: SharedListener<T>[] = [];
-    let _value: T = defaultValue;
     const get = () => _value;
     const set = (value: T) => {
-        _value = value;
-        _subs.forEach(listener => listener(_value));
+        if (_value !== value) {
+            _value = value;
+            for (const listener of _subs) {
+                listener(_value);
+            }
+        }
     }
     const on = (listener: SharedListener<T>) => {
-        _subs.push(listener);
+        _subs = [ ..._subs, listener ];
         return listener;
     }
     const off = (listener: SharedListener<T>) => {
-        _subs = _subs.filter(l => l != listener);
+        _subs = _subs.filter(l => l !== listener);
     }
     return { get, set, on, off }
 }
 
-export const createStorageShared = <T>(storage: Storage, key: string, defaultValue: T): Shared<T> => {
-    const type = (typeof defaultValue);
-    const item = `__shared_${key}_${type}`;
-    const json = storage.getItem(item);
-    const shared = (() => {
-        if (json) {
-            return createShared<T>((() => {
-                try { return (JSON.parse(json)?.value ?? defaultValue); }
-                catch (e) { return defaultValue; }
-            })());
-        } else {
-            return createShared<T>(defaultValue);
-        }
-    })()
-    shared.on(value => storage.setItem(item, JSON.stringify({ value })));
-    return shared;
-}
-
-export type SharedHook<T> = [ T, (value: T) => void ];
+export type SharedHook<T> = [T, (value: T) => void ];
 
 export const useShared = <T>(shared: Shared<T>): SharedHook<T> => {
-    const [ value, setValue ] = useState<T>(shared.get());
+    const [ , setDummy ] = useState<number>(0);
     useEffect(() => {
-        const listener = shared.on((value: T) => setValue(value));
+        const listener = shared.on(() => setDummy(prev => prev + 1));
         return () => shared.off(listener);
     }, [shared]);
-    return [ value, (value: T) => shared.set(value) ];
+    return [
+        shared.get(),
+        (value: T) => shared.set(value)
+    ];
 }
